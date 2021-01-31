87930d9dfa154e9e796058527a152e85a7948711
jdanielue <jdurregoe@gmail.com>
2021-01-31 15:53:40 -0500

    02 | code update | code running and output good betty  v3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,22 +8,24 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *nodomenor, *tmp, *nodosiguiente;
+	listint_t *current = *list, *nodomenor, *tmp = current, *nodosiguiente;
 
 	current = *list;
 	tmp = current;
 
-	while(current->next)
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	while (current->next)
 	{
 		nodosiguiente = current->next;
-		if (current->n > nodosiguiente->n)
+		if (nodosiguiente->n < current->n)
 		{
-			while(tmp != NULL && nodosiguiente->n < tmp->n)
+			while (tmp != NULL && nodosiguiente->n < tmp->n)
 			{
-				if (nodosiguiente->next != NULL && tmp->prev)
+				if (nodosiguiente->next != NULL && tmp->prev != NULL)
 				{
 					nodomenor = nodosiguiente;
-
 					tmp->next = tmp->next->next;
 					tmp->prev->next = nodomenor;
 					nodomenor->next = tmp;
@@ -32,22 +34,20 @@ void insertion_sort_list(listint_t **list)
 					tmp->next->prev = tmp;
 					print_list(*list);
 				}
-				else if (tmp->prev == NULL)
+				else if (tmp->prev == NULL && nodosiguiente->next != NULL)
 				{
 					nodomenor = nodosiguiente;
-
 					tmp->next = tmp->next->next;
-                    nodomenor->next = tmp;
-                    nodomenor->prev = NULL;
-                    tmp->prev = nodomenor;
-                    tmp->next->prev = tmp;
-					*list =nodomenor;
+					nodomenor->next = tmp;
+					nodomenor->prev = NULL;
+					tmp->prev = nodomenor;
+					tmp->next->prev = tmp;
+					*list = nodomenor;
 					print_list(*list);
 				}
-				else if (nodosiguiente->next == NULL)
+				else if (nodosiguiente->next == NULL && tmp->prev != NULL)
 				{
 					nodomenor = nodosiguiente;
-
 					tmp->next = NULL;
 					tmp->prev->next = nodomenor;
 					nodomenor->next = tmp;
@@ -55,8 +55,16 @@ void insertion_sort_list(listint_t **list)
 					tmp->prev = nodomenor;
 					print_list(*list);
 				}
+				else if (nodosiguiente->next == NULL && tmp->prev == NULL)
+				{
+					nodomenor->prev->next = nodomenor->next;
+					nodomenor->next = nodomenor->prev;
+					nodomenor->prev = nodomenor->prev->prev;
+					nodomenor->next->prev = nodomenor;
+					*list = nodomenor;
+					print_list(*list);
+				}
 			tmp = nodosiguiente->prev;
-
 			}
 			tmp = current;
 		}
