9457df53a88943f08b69c2ccb5f60539d91adc39
klich1984 <2239@holbertonschool.com>
2021-02-02 15:36:31 -0500

    02 | code update | test 2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,43 +1,54 @@
 #include "sort.h"
 
-void fuc_quicsort_mia(int *array, int izq, int der)
+
+
+void quick_sort(int *array, size_t size)
 {
+	quick_sort_lomuto(array, 0, size - 1);
+	print_array(array, size);
+}
 
-	int i = izq, j = der, tmp;
-	int piv = array[(izq + der) / 2];
+void quick_sort_lomuto(int *array, int low, int hi)
+{
+	int pos;
 
-	printf("entre aca fuc_quicsort_mia\n");
-	printf("izq = %d der = %d\n", izq, der);
-	printf("i = %d j = %d piv = %d\n", i, j, piv);
-	while(i <= j)
+	if(low < hi)
 	{
-		printf("entre aca\n");
-		while (array[i] < piv)
-			i++;
-		while (array[j] > piv)
-			j--;
-		if (i <= j)
+		pos = parttion(array, low, hi);
+		quick_sort_lomuto(array, low, pos - 1);
+		quick_sort_lomuto(array, pos + 1, hi);
+	}
+}
+
+int parttion(int *array, int low, int hi)
+{
+	int piv = array[hi], i = low, j = 0, tmp = 0;
+
+	for( j = low; j < hi; j++)
+	{
+		
+		if (array[j] < piv)
 		{
 			tmp = array[i];
 			array[i] = array[j];
 			array[j] = tmp;
+			print_array(array, 10);
 			i++;
-			j--;
 		}
 	}
-	if (izq < j)
-		fuc_quicsort_mia(array, izq, j);
-	if (i < der)
-		fuc_quicsort_mia(array, i, der);
+	tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+	print_array(array, 10);
+
+	return (i);
 }
 
-void quick_sort(int *array, size_t size)
+/*void swap(int *array, int *a, int *b)
 {
-	int izq = array[0], der = array[size - 1];
-	(void)array;
-	(void)size;
+	int tmp;
 
-	printf("izq = %d der = %d\n", izq, der);
-	fuc_quicsort_mia(array, 0, size - 1);
-	print_array(array, size);
-}
+	tmp = array[a];
+	array[a] = array[b];
+	array[b] = tmp;
+}*/
