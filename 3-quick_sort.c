8d09fe462782d4d490a2fe5fd57583bb2f8a678f
klich1984 <2239@holbertonschool.com>
2021-02-02 21:57:37 -0500

    04 | code update | update betty all the functions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,17 +1,36 @@
 #include "sort.h"
 
-
+/**
+ * quick_sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: array of imtegers
+ * @size: size of the array
+ * Return: Notong
+ */
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
+
 	quick_sort_lomuto(array, size, 0, size - 1);
 }
+
+/**
+ * quick_sort_lomuto - function that sort array with implement Lomuto
+ * @array: array of imtegers
+ * @size: size of the array
+ * @low: less elemet of the array
+ * @hi: high elemt of the array
+ * Return: Noting
+ */
+
 
 void quick_sort_lomuto(int *array, size_t size, int low, int hi)
 {
 	int pos;
 
-	if(low < hi)
+	if (low < hi)
 	{
 		pos = partition(array, size, low, hi);
 		quick_sort_lomuto(array, size,  low, pos - 1);
@@ -19,13 +38,21 @@ void quick_sort_lomuto(int *array, size_t size, int low, int hi)
 	}
 }
 
+/**
+ * partition - function that separate of the elemnts of the array
+ * @array: array of imtegers
+ * @size: size of the array
+ * @low: less elemet of array
+ * @hi: high elemt of array
+ * Return: position current of array
+ */
+
 int partition(int *array, size_t size, int low, int hi)
 {
 	int piv = array[hi], i = low, j = 0;
 
-	for( j = low; j < hi; j++)
+	for (j = low; j < hi; j++)
 	{
-
 		if (array[j] < piv)
 		{
 			swap(array, size, i, j);
@@ -36,6 +63,15 @@ int partition(int *array, size_t size, int low, int hi)
 
 	return (i);
 }
+
+/**
+ * swap - function that change elemnts
+ * @array: array of imtegers
+ * @size: size of the array
+ * @a: element to change
+ * @b: element to change
+ * Return: Noting
+ */
 
 void swap(int *array, size_t size, int a, int b)
 {
