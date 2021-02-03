84ef91b4390ca70e89bb8f8fc31810dec7e10212
klich1984 <2239@holbertonschool.com>
2021-02-02 20:17:57 -0500

    03 | code update | code running and output good

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,51 +4,48 @@
 
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_lomuto(array, 0, size - 1);
-	print_array(array, size);
+	quick_sort_lomuto(array, size, 0, size - 1);
 }
 
-void quick_sort_lomuto(int *array, int low, int hi)
+void quick_sort_lomuto(int *array, size_t size, int low, int hi)
 {
 	int pos;
 
 	if(low < hi)
 	{
-		pos = parttion(array, low, hi);
-		quick_sort_lomuto(array, low, pos - 1);
-		quick_sort_lomuto(array, pos + 1, hi);
+		pos = partition(array, size, low, hi);
+		quick_sort_lomuto(array, size,  low, pos - 1);
+		quick_sort_lomuto(array, size, pos + 1, hi);
 	}
 }
 
-int parttion(int *array, int low, int hi)
+int partition(int *array, size_t size, int low, int hi)
 {
-	int piv = array[hi], i = low, j = 0, tmp = 0;
+	int piv = array[hi], i = low, j = 0;
 
 	for( j = low; j < hi; j++)
 	{
-		
+
 		if (array[j] < piv)
 		{
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
-			print_array(array, 10);
+			swap(array, size, i, j);
 			i++;
 		}
 	}
-	tmp = array[i];
-	array[i] = array[j];
-	array[j] = tmp;
-	print_array(array, 10);
+	swap(array, size, i, j);
 
 	return (i);
 }
 
-/*void swap(int *array, int *a, int *b)
+void swap(int *array, size_t size, int a, int b)
 {
 	int tmp;
 
-	tmp = array[a];
-	array[a] = array[b];
-	array[b] = tmp;
-}*/
+	if (array[a] != array[b])
+	{
+		tmp = array[a];
+		array[a] = array[b];
+		array[b] = tmp;
+		print_array(array, size);
+	}
+}
